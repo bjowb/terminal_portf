23a110d122d86ccbbb6b2c991bc9a4114be91efb
bjowb <pulktchhr@gmail.com>
2026-06-15 18:27:25 +0530

    Made a skeletal design

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className='app-container'>
+            <h1>Kitty_Terminal_Portfolio</h1>
+        </div>
+    )
+}
+
+export default App
